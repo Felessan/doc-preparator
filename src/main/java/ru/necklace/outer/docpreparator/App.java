60e16202a529a23d2cb9d7fe04f8098c48a59f28
Felessan <netstorm2@rambler.ru>
2023-12-13 14:15:38 +0300

    add main window, icon

--- src/main/java/ru/necklace/outer/docpreparator/App.java
+++ src/main/java/ru/necklace/outer/docpreparator/App.java
@@ -1,6 +1,37 @@
 package ru.necklace.outer.docpreparator;
 
-public class App {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class App extends Application {
     public static void main(String[] args) {
+        App.launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initPrimary(primaryStage);
+        primaryStage.show();
+    }
+
+    private void initPrimary(Stage primaryStage) throws IOException {
+        try (InputStream iconStream = getClass().getResourceAsStream("/icons/MainTitle.png")) {
+            Image headerImage = new Image(iconStream);
+            primaryStage.getIcons().add(headerImage);
+        }
+
+        primaryStage.setTitle("Заполнятель допусков");
+
+        Label label = new Label("Hello World");
+        label.setAlignment(Pos.CENTER);
+        Scene primary = new Scene(label);
+        primaryStage.setScene(primary);
+
     }
 }
